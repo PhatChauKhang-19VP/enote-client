0dc142d1c386e09d05daf60fb0ef324df5693ad2
Khang Pham <vkhangpham@gmail.com>
2022-07-01 15:49:43 +0700

    add controller for view notes and note details

--- src/main/java/pck/enote/controller/ViewNoteDetailsController.java
+++ src/main/java/pck/enote/controller/ViewNoteDetailsController.java
@@ -1,42 +1,83 @@
 package pck.enote.controller;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import pck.enote.Enote;
 import pck.enote.api.API;
 import pck.enote.api.req.GetNoteReq;
 import pck.enote.api.res.GetNoteRes;
 import pck.enote.fe.model.Note;
 
+import java.io.ByteArrayInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ViewNoteDetailsController implements Initializable {
-
     public TextField tfId;
     public TextField tfFileName;
     public TextField tfFileType;
     public TextField tfCreatedAt;
     public Pane content;
+    public Label notification;
+    public Button btnBack;
+    public Button btnDownload;
+    private String username;
+    private Integer noteId;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        getNoteDetails("phat", 1);
+        getNoteDetails();
+        btnBack.setOnAction(event -> {
+            Enote.gotoViewNotesPage(username);
+        });
     }
 
-    private void getNoteDetails(String username, Integer noteId) {
-        GetNoteRes res = (GetNoteRes) API.sendReq(new GetNoteReq(username, noteId));
+    private void getNoteDetails() {
+        GetNoteRes res = (GetNoteRes) API.sendReq(new GetNoteReq(this.username, this.noteId));
         Note note = res.getNote();
         tfId.setText(String.valueOf(note.getId()));
+
+        // split the path on splash to get file name
         String[] splitFileName = note.getUri().split("/");
         tfFileName.setText(splitFileName[splitFileName.length - 1]);
+
         tfFileType.setText(note.getType());
         tfCreatedAt.setText(note.getCreatedAt());
-        content.getChildren().clear();
-        content.getChildren().add(
-                new ImageView(new Image(note.getUri()))
-        );
+
+        // display content
+        String type = note.getType();
+        if (type.contains("plain")) {
+            content.getChildren().clear();
+            content.getChildren().add(
+                    new Label(new String(note.getContent()))
+            );
+        } else if (type.contains("image")) {
+            content.getChildren().clear();
+            Image img = new Image(new ByteArrayInputStream(note.getContent()));
+            content.getChildren().add(
+                    new ImageView(img)
+            );
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(Integer noteId) {
+        this.noteId = noteId;
     }
 }
--- src/main/java/pck/enote/controller/ViewNotesController.java
+++ src/main/java/pck/enote/controller/ViewNotesController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Callback;
+import pck.enote.Enote;
 import pck.enote.api.API;
 import pck.enote.api.req.GetNoteListReq;
 import pck.enote.api.res.GetNoteListRes;
@@ -16,21 +17,22 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ViewNotesController implements Initializable {
-
     public TableView<Note> tableView;
     public TableColumn<Note, Integer> colId;
     public TableColumn<Note, String> colFileName;
     public TableColumn<Note, String> colFileType;
     public TableColumn colBtn;
     public TableColumn<Note, String> colCreatedAt;
+    private String username = "phat1";
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.getNotes(username);
         colId.setCellValueFactory(new PropertyValueFactory<Note, Integer>("id"));
         colFileName.setCellValueFactory(new PropertyValueFactory<Note, String>("uri"));
         colFileType.setCellValueFactory(new PropertyValueFactory<Note, String>("type"));
         colCreatedAt.setCellValueFactory(new PropertyValueFactory<Note, String>("createdAt"));
-
+        String username = this.username;
         Callback<TableColumn<Note, String>, TableCell<Note, String>> cellFactory = new Callback<>() {
             @Override
             public TableCell call(final TableColumn<Note, String> param) {
@@ -42,28 +44,34 @@ public class ViewNotesController implements Initializable {
                         super.updateItem(item, empty);
                         if (empty) {
                             setGraphic(null);
-                            setText(null);
                         } else {
                             btn.setOnAction(event -> {
-                                Note Note = getTableView().getItems().get(getIndex());
-                                System.out.println("select Note " + Note.getUri());
-//                                gotoListProduct(Note);
+                                Note note = getTableView().getItems().get(getIndex());
+                                System.out.println("select Note " + note.getUri());
+                                Enote.gotoViewNoteDetailsPage(username, note.getId());
                             });
                             setGraphic(btn);
-                            setText(null);
                         }
+                        setText(null);
                     }
                 };
                 return cell;
             }
         };
         colBtn.setCellFactory(cellFactory);
-        this.getNotes("phat1");
     }
 
     private void getNotes(String username) {
         GetNoteListRes res = (GetNoteListRes) API.sendReq(new GetNoteListReq(username));
         tableView.getItems().clear();
         tableView.getItems().addAll(res.getNoteList().values());
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
